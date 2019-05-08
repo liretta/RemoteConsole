abb636fb2b609cf28c13571219e1ad55de467a9f
Hoffman Roma <hoffroma24@gmail.com>
2019-05-08 12:46:24 +0300

    wrong problem solution

--- RemoteConsole/Source.cpp
+++ RemoteConsole/Source.cpp
@@ -1,82 +1,23 @@
 #include "server_executor.h"
 #include <string>
 #include <iostream>
-#include "ms_executor.h"
 
 int main()
 {
 	ServerExecutor executor;
 	if (executor.initialize())
 	{
+		executor.execute(std::wstring(L"dir"));
 
-		executor.execute(std::wstring(L"help"));
-
-		std::wcout << executor.getResult() << std::endl;
+		auto text = executor.getResult();
+		std::wcout << text.size() << '\n' << text << std::endl;
 	}
 	else
 	{
 		std::cerr << "cannot initialize executor" << std::endl;
 	}
 
-
-
-
-
-
-	SECURITY_ATTRIBUTES saAttr;
-
-	printf("\n->Start of parent execution.\n");
-
-	// Set the bInheritHandle flag so pipe handles are inherited. 
-
-	saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
-	saAttr.bInheritHandle = TRUE;
-	saAttr.lpSecurityDescriptor = NULL;
-
-	// Create a pipe for the child process's STDOUT. 
-
-	if (!CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0))
-		ErrorExit(PTSTR("StdoutRd CreatePipe"));
-
-	// Ensure the read handle to the pipe for STDOUT is not inherited.
-
-	if (!SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0))
-		ErrorExit(PTSTR("Stdout SetHandleInformation"));
-
-	// Create a pipe for the child process's STDIN. 
-
-	if (!CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0))
-		ErrorExit(PTSTR("Stdin CreatePipe"));
-
-	// Ensure the write handle to the pipe for STDIN is not inherited. 
-
-	if (!SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0))
-		ErrorExit(PTSTR("Stdin SetHandleInformation"));
-
-	// Create the child process. 
-
-	CreateChildProcess();
-
-	// Get a handle to an input file for the parent. 
-	// This example assumes a plain text file and uses string output to verify data flow. 
-
-
-	// Write to the pipe that is the standard input for a child process. 
-	// Data is written to the pipe's buffers, so it is not necessary to wait
-	// until the child process is running before writing data.
-
-	WriteToPipe();
-	printf("\n->Contents of written to child STDIN pipe.\n");
-
-	// Read from pipe that is the standard output for child process. 
-
-	printf("\n->Contents of child process STDOUT:\n\n");
-	ReadFromPipe();
-
-	printf("\n->End of parent execution.\n");
-
-	// The remaining open handles are cleaned up when this process terminates. 
-	// To avoid resource leaks in a larger application, close handles explicitly. 
+	
 
 	system("PAUSE");
 	return 0;
---
+++ RemoteConsole/server_executor.cpp
@@ -0,0 +1,131 @@
+#include "server_executor.h"
+
+static wchar_t* ANSItoUNICODE(char* line)
+{
+	std::size_t
+		length = strlen(line) + 1,
+		size_needed = MultiByteToWideChar(CP_OEMCP, 0, line, length, nullptr, 0);
+
+	wchar_t* w_line =
+		static_cast<wchar_t*>(LocalAlloc(LPTR, sizeof(wchar_t) * size_needed));
+
+	MultiByteToWideChar(CP_OEMCP, 0, line, length, w_line, size_needed);
+	return w_line;
+}
+
+
+
+bool ServerExecutor::initialize()
+{
+	m_security_attributes.nLength = sizeof(SECURITY_ATTRIBUTES);
+	m_security_attributes.bInheritHandle = TRUE;
+	m_security_attributes.lpSecurityDescriptor = nullptr;
+
+	bool is_initialized = true;
+
+	if (CreatePipe(&m_child_out_read,
+	               &m_child_out_write, &m_security_attributes, 0) != TRUE)
+	{
+		is_initialized = false;
+	}
+
+	// Ensure the read handle to the pipe for STDOUT is not inherited.
+	if (SetHandleInformation(m_child_out_read,
+	                         HANDLE_FLAG_INHERIT, 0) != TRUE)
+	{
+		is_initialized = false;
+	}
+
+	return is_initialized;
+}
+
+void ServerExecutor::execute(const std::wstring& command)
+{
+	wchar_t w_command[MAX_PATH];
+	wcscpy_s(w_command, command.data());
+
+	PROCESS_INFORMATION process_information;
+	ZeroMemory(&process_information, sizeof(PROCESS_INFORMATION));
+
+	STARTUPINFOW startup_info;
+	ZeroMemory(&startup_info, sizeof(STARTUPINFOW));
+	// Set up members of the STARTUPINFOW structure. 
+	// This structure specifies the STDIN and STDOUT handles for redirection.
+	startup_info.cb = sizeof(STARTUPINFOW);
+	startup_info.hStdError = m_child_out_write;
+	startup_info.hStdOutput = m_child_out_write;
+	startup_info.hStdInput = nullptr;
+	startup_info.dwFlags |= STARTF_USESTDHANDLES;
+
+	// Create the child process. 
+	BOOL is_created = FALSE;
+	is_created = CreateProcessW(
+		nullptr,
+		w_command,				// command line 
+		nullptr,				// process security attributes 
+		nullptr,				// primary thread security attributes 
+		TRUE,					// handles are inherited 
+		0,						// creation flags 
+		nullptr,				// use parent's environment 
+		nullptr,				// use parent's current directory 
+		&startup_info,			// STARTUPINFOW pointer 
+		&process_information	// receives PROCESS_INFORMATION 
+	);
+
+	// If an error occurs, exit the application. 
+	if (is_created != TRUE)
+	{
+		std::string message;
+
+		DWORD error = GetLastError();
+		DWORD written_symbols_count;
+		switch (error)
+		{
+		case 2:
+			message = "Invalid command";
+			WriteFile(m_child_out_write, message.data(),
+			          message.size() + 1, &written_symbols_count, nullptr);
+			break;
+		}
+	}
+	else
+	{
+		// Close handles to the child process and its primary thread.
+		// Some applications might keep these handles to monitor the status
+		// of the child process, for example. 
+		CloseHandle(process_information.hProcess);
+		CloseHandle(process_information.hThread);
+		CloseHandle(m_child_out_write);
+	}
+}
+
+std::wstring ServerExecutor::getResult() const
+{
+	std::wstring result;
+
+	DWORD read_symbols_count = 0;
+	char buffer[BUFSIZE];
+	BOOL is_read = FALSE;
+
+	wchar_t* w_tmp_buffer = nullptr;
+	do
+	{
+		memset(buffer, 0, BUFSIZE);
+
+		is_read = ReadFile(
+			m_child_out_read, buffer, BUFSIZE,
+			&read_symbols_count, nullptr);
+
+		//buffer[read_symbols_count] = 0;
+
+		w_tmp_buffer = ANSItoUNICODE(buffer);
+		result.append(w_tmp_buffer);
+		//std::wcout << read_symbols_count << ' ' << result << L"\n\n\n";
+
+		LocalFree(w_tmp_buffer);
+	}
+	while (is_read == TRUE && read_symbols_count != 0);
+
+
+	return result;
+}
