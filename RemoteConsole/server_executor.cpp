f26ceb345216864d781c017c8bcab827a62a1f00
Hoffman Roma <hoffroma24@gmail.com>
2019-05-10 11:11:09 +0300

    updated code style; add feature to run cmd commands

--- RemoteConsole/server_executor.cpp
+++ RemoteConsole/server_executor.cpp
@@ -6,13 +6,20 @@ static wchar_t* ANSItoUNICODE(char* line)
 		length = int(strlen(line)) + 1,
 		size_needed = MultiByteToWideChar(CP_OEMCP, 0, line, length, nullptr, 0);
 
+	// allocate memory for the Unicode line
 	wchar_t* w_line =
 		static_cast<wchar_t*>(LocalAlloc(LPTR, sizeof(wchar_t) * size_needed));
 
+	// transform
 	MultiByteToWideChar(CP_OEMCP, 0, line, length, w_line, size_needed);
+
 	return w_line;
 }
 
+static std::wstring COMMANDLINEtoCMDCOMMAND(const std::wstring& command_line)
+{
+	return std::wstring(L"cmd /C ") + command_line;
+}
 
 
 bool ServerExecutor::initialize()
@@ -29,7 +36,7 @@ bool ServerExecutor::initialize()
 		is_initialized = false;
 	}
 
-	// Ensure the read handle to the pipe for STDOUT is not inherited.
+	// ensure the read handle to the pipe for STDOUT is not inherited
 	if (SetHandleInformation(m_child_out_read,
 	                         HANDLE_FLAG_INHERIT, 0) != TRUE)
 	{
@@ -39,27 +46,29 @@ bool ServerExecutor::initialize()
 	return is_initialized;
 }
 
+
 void ServerExecutor::execute(const std::wstring& command)
 {
+	std::wstring cmd_command = COMMANDLINEtoCMDCOMMAND(command);
+
 	wchar_t w_command[MAX_PATH];
-	wcscpy_s(w_command, command.data());
+	wcscpy_s(w_command, cmd_command.data());
 
 	PROCESS_INFORMATION process_information;
 	ZeroMemory(&process_information, sizeof(PROCESS_INFORMATION));
 
 	STARTUPINFOW startup_info;
 	ZeroMemory(&startup_info, sizeof(STARTUPINFOW));
-	// Set up members of the STARTUPINFOW structure. 
-	// This structure specifies the STDIN and STDOUT handles for redirection.
+	// set up members of the STARTUPINFOW structure
+	// this structure specifies the STDIN and STDOUT handles for redirection
 	startup_info.cb = sizeof(STARTUPINFOW);
 	startup_info.hStdError = m_child_out_write;
 	startup_info.hStdOutput = m_child_out_write;
 	startup_info.hStdInput = nullptr;
 	startup_info.dwFlags |= STARTF_USESTDHANDLES;
 
-	// Create the child process. 
-	BOOL is_created = FALSE;
-	is_created = CreateProcessW(
+	// create the child process
+	BOOL is_created = CreateProcessW(
 		nullptr,
 		w_command,				// command line 
 		nullptr,				// process security attributes 
@@ -72,35 +81,20 @@ void ServerExecutor::execute(const std::wstring& command)
 		&process_information	// receives PROCESS_INFORMATION 
 	);
 
-	// If an error occurs, exit the application. 
 	if (is_created != TRUE)
 	{
-		std::string message;
-
-		DWORD error = GetLastError();
-		DWORD written_symbols_count;
-		switch (error)
-		{
-		case 2:
-			message = "Invalid command";
-			break;
-		}
-
-		if (error != 0)
-		{
-			WriteFile(m_child_out_write, message.data(),
-				DWORD(message.size()) + 1, &written_symbols_count, nullptr);
-		}
+		send_error_message();
 	}
 	else
 	{
-		// Close handles to the child process and its primary thread.
-		// Some applications might keep these handles to monitor the status
-		// of the child process, for example. 
+		// close handles to the child process and its primary thread;
+		// some applications might keep these handles to monitor the status
+		// of the child process, for example
 		CloseHandle(process_information.hProcess);
 		CloseHandle(process_information.hThread);
 	}
 }
+
 
 std::wstring ServerExecutor::getResult() const
 {
@@ -110,6 +104,7 @@ std::wstring ServerExecutor::getResult() const
 	char buffer[BUFSIZE];
 	BOOL is_read = FALSE;
 
+	// close writing side of pipe to be able to read
 	CloseHandle(m_child_out_write);
 
 	wchar_t* w_tmp_buffer = nullptr;
@@ -117,9 +112,8 @@ std::wstring ServerExecutor::getResult() const
 	{
 		memset(buffer, 0, BUFSIZE);
 
-		is_read = ReadFile(
-			m_child_out_read, buffer, BUFSIZE,
-			&read_symbols_count, nullptr);
+		is_read = ReadFile(m_child_out_read, buffer,
+						   BUFSIZE, &read_symbols_count, nullptr);
 
 		w_tmp_buffer = ANSItoUNICODE(buffer);
 		result.append(w_tmp_buffer);
@@ -128,6 +122,30 @@ std::wstring ServerExecutor::getResult() const
 	}
 	while (is_read == TRUE && read_symbols_count != 0);
 
-
 	return result;
+}
+
+
+void ServerExecutor::send_error_message()
+{
+	DWORD error = GetLastError();
+
+	if (error != 0)
+	{
+		std::string message;
+
+		switch (error)
+		{
+		case 2:
+			message = "Invalid command";
+			break;
+		default:
+			message = "Unhandled error";
+			break;
+		}
+
+		DWORD written_symbols_count; //< cannot pass nullptr, so need a variable
+		WriteFile(m_child_out_write, message.data(),
+				  DWORD(message.size()) + 1, &written_symbols_count, nullptr);
+	}
 }
--- RemoteConsole/server_executor.h
+++ RemoteConsole/server_executor.h
@@ -6,19 +6,54 @@
 #define BUFSIZE 4096
 
 
+class IServerExecutor
+{
+public:
+	virtual ~IServerExecutor() = default;
+
+	
+	virtual bool			initialize()							= 0;
+	virtual void			execute(const std::wstring& command)	= 0;
+	virtual std::wstring	getResult() const						= 0;
+};
+
+class ServerExecutorStub : public IServerExecutor
+{
+	//
+};
 
 
-class ServerExecutor
+
+class ServerExecutor : public IServerExecutor
 {
 public:
 
-	ServerExecutor() = default;
+	ServerExecutor()			= default;
+	virtual ~ServerExecutor()	= default;
 
-	bool initialize();
-	void execute(const std::wstring& command);
-	std::wstring getResult() const;
+	/*!
+	 * Set up security attributes and create a pipe.
+	 * @return initialization status.
+	 */
+	bool			initialize()							override;
+
+	/*!
+	 * Execute Unicode command in a new sub-process.
+	 */
+	void			execute(const std::wstring& command)	override;
+
+	/*!
+	 * Get output from the sub-process and transform it to Unicode.
+	 * @return Unicode stdout of the sub-process.
+	 */
+	std::wstring	getResult() const						override;
 
 private:
+
+	/*!
+	 * Write the error message to sub-process output if the error exists.
+	 */
+	void send_error_message();
 
 	SECURITY_ATTRIBUTES
 		m_security_attributes;
